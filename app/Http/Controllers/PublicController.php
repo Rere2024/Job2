bcebf3f8e0b6c3c7c58af42cca4602e45ec118ba
Rere2024 <www.rehabmonir2024@gmail.com>
2024-08-24 09:20:51 +0300

    job

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 class PublicController extends Controller
 {
+    public function index()
+    {
+         return view('index');
+     }
+
+
     public function about()
     {
          return view('about');
@@ -23,7 +29,7 @@ class PublicController extends Controller
 
        public function jobDetails()
        {
-            return view('job-Details');
+            return view('job_Details');
         }
 
         public function jobList()
@@ -36,6 +42,57 @@ class PublicController extends Controller
               return view('testimonial');
           }
 
+
+          
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show()
+    // public function show(string $id)
+    {
+        return view('job-detail');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\JobController;
+use App\Http\Controllers\Admin\JobController;
 use App\Http\Controllers\PublicController;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('index', [JobController::class, 'index']);
+// Route::get('index', [JobController::class, 'index'])->name('index');
+// Route::get('create', [JobController::class, 'create'])->name('jobs.create');
+// Route::get('', [JobController::class, 'store'])->name('jobs.store');
+// Route::post('create', [JobController::class, 'create'])->name('jobs.create');
 
-
-
-Route::get('about', [PublicController::class, 'about']);
-Route::get('category', [PublicController::class, 'category']);
-Route::get('contact', [PublicController::class, 'contact']);
-Route::get('jobDetails', [PublicController::class, 'jobDetails']);
-Route::get('jobList', [PublicController::class, 'jobList']);
-Route::get('testimonil', [PublicController::class, 'testimonial']);
+Route::get('index', [PublicController::class, 'index'])->name('index');
+Route::get('about', [PublicController::class, 'about'])->name('about');
+Route::get('category', [PublicController::class, 'category'])->name('category');
+Route::get('contact', [PublicController::class, 'contact'])->name('contact');
+Route::get('jobDetails', [PublicController::class, 'jobDetails'])->name('jobDetails');
+Route::get('jobList', [PublicController::class, 'jobList'])->name('jobList');
+Route::get('testimonil', [PublicController::class, 'testimonial'])->name('testimonial');
